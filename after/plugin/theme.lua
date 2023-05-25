dfb71cb5b90f212f33b5232585ed820cea5642f9
Aatish Sai <meatish.sai@gmail.com>
2023-05-26 00:42:02 +0545

    WIP

--- after/plugin/theme.lua
+++ after/plugin/theme.lua
@@ -1,13 +1,10 @@
-local status, theme = pcall(require, "rose-pine")
+local status, theme = pcall(require, "dracula")
 
 if not status then
     return
 end
 
 theme.setup({
-    style="night",
-    show_end_of_buffer = false,
-    transparent_bg = true
 })
 
-vim.cmd[[colorscheme rose-pine]]
+vim.cmd[[colorscheme dracula]]

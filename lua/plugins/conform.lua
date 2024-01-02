04c5bb852ddc5eec0874768c1e78118e47afb755
Aatish Sai <meatish.sai@gmail.com>
2024-01-02 12:29:05 +0545

    fixes

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,50 +1,35 @@
 return {
-    "stevearc/conform.nvim",
-    cmd = { "ConformInfo" },
-    keys = {
-        {
-            "<leader>f",
-            function()
-                require("conform").format({ async = true, lsp_fallback = true })
-            end,
-            mode = "",
-            desc = "Format buffer",
-        },
-    },
-    opts = {
-        format_by_ft = {
-            javascript = { { "prettierd", "prettier" } },
-            typescript = { { "prettierd", "prettier" } },
-            javascriptreact = { "prettierd" },
-            typescriptreact = { "prettierd" },
-            css = { "prettierd" },
-            html = { "prettierd" },
-            json = { "prettierd" },
-            markdown = { "prettierd" },
-            lua = { "stylua" },
-            python = { "isort", "black" },
-            php = { "pint" },
-            yaml = { "yamlfmt" }
-        },
-        format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true
-        },
-        formatters = {
-            injected = { options = { ignore_errors = true } },
-
-            pint = {
-                meta = {
-                },
-                command = function()
-                    require("conform").utils.find_executable({
-                        vim.fn.stdpth("data") .. "/mason/bin/pint",
-                        "vendor/bin/pint",
-                    }, "pint")
-                end,
-                args = { "$FILENAME" },
-                stdin = false,
-            }
-        }
-    },
+	"stevearc/conform.nvim",
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	opts = {
+		formatters_by_ft = {
+			javascript = { { "prettierd", "prettier" } },
+			typescript = { { "prettierd", "prettier" } },
+			javascriptreact = { "prettierd" },
+			typescriptreact = { "prettierd" },
+			css = { "prettierd" },
+			html = { "prettierd" },
+			json = { "prettierd" },
+			markdown = { "prettierd" },
+			lua = { "stylua" },
+			python = { "isort", "black" },
+		},
+		format_on_save = {
+			timeout_ms = 500,
+			lsp_fallback = true,
+		},
+		formatters = {
+			injected = { options = { ignore_errors = true } },
+		},
+	},
 }
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,86 +1,86 @@
 return {
-    {
-        "rcarriga/nvim-notify",
-        opts = {
-            timeout = 3000
-        },
-        init = function()
-            vim.notify = require("notify")
-        end
-    },
-    {
-        "akinsho/bufferline.nvim",
-        event = "VeryLazy",
-        keys = {},
-        opts = {
-            options = {
-                indicator = {
-                    style = 'underline'
-                },
-                show_buffer_close_icons = true,
-                diagnostics = "nvim_lsp",
-                offsets = {
-                    {
-                        filetype = "NvimTree",
-                        text = "File Explorer",
-                        text_align = "center",
-                        highlight = "Directory",
-                        separator = false
-                    }
-                },
-            },
-        },
-    },
-    {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        opts = {
-            options = {
-                icons_enabled = false,
-                globalstatus = true,
-                theme = 'auto',
-            },
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { "branch", 'diff' },
-                lualine_c = {
-                    { 'filename', file_status = true, path = 1 }
-                },
-                lualine_x = {
-                    { 'diagnostics', sources = { 'nvim_diagnostic' } },
-                    'encoding',
-                    'filetype'
-                },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' }
-            }
-        },
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        opts = {
-            indent = {
-                char = "|",
-                tab_char = {'a','b','c'}
-            },
-            scope = {
-                enabled = true
-            },
-            exclude = {
-                filetypes = {
-                    "help",
-                    "lazyterm",
-                    "Trouble",
-                    "lazy",
-                    "mason",
-                    "notify",
-                    "toggleterm",
-                    "NvimTree",
-                    "dashoboard",
-                    "float"
-                }
-            }
-        },
-        main = "ibl"
-    }
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			timeout = 3000,
+		},
+		init = function()
+			vim.notify = require("notify")
+		end,
+	},
+	{
+		"akinsho/bufferline.nvim",
+		event = "VeryLazy",
+		keys = {},
+		opts = {
+			options = {
+				indicator = {
+					style = "underline",
+				},
+				show_buffer_close_icons = true,
+				diagnostics = "nvim_lsp",
+				offsets = {
+					{
+						filetype = "NvimTree",
+						text = "File Explorer",
+						text_align = "center",
+						highlight = "Directory",
+						separator = false,
+					},
+				},
+			},
+		},
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		opts = {
+			options = {
+				icons_enabled = false,
+				globalstatus = true,
+				theme = "auto",
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff" },
+				lualine_c = {
+					{ "filename", file_status = true, path = 1 },
+				},
+				lualine_x = {
+					{ "diagnostics", sources = { "nvim_diagnostic" } },
+					"encoding",
+					"filetype",
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		},
+	},
+	{
+		"lukas-reineke/indent-blankline.nvim",
+		opts = {
+			indent = {
+				char = "|",
+				tab_char = { "|" },
+			},
+			scope = {
+				enabled = true,
+			},
+			exclude = {
+				filetypes = {
+					"help",
+					"lazyterm",
+					"Trouble",
+					"lazy",
+					"mason",
+					"notify",
+					"toggleterm",
+					"NvimTree",
+					"dashoboard",
+					"float",
+				},
+			},
+		},
+		main = "ibl",
+	},
 }

65ab704f9cd9f7d7c523faf85f55125266327a3a
Aatish Sai <meatish.sai@gmail.com>
2024-01-24 00:53:39 +0545

    change colorscheme to rosepine

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,17 +1,15 @@
 return {
-    {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        priority = 1000,
-        opts = {
-            transparent = true,
-            styles = {
-                sidebars = "transparent",
-                floats = "transparent",
-            }
-        },
-        init = function()
-            vim.cmd.colorscheme("tokyonight")
-        end
-    }
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		opts = {
+			variant = "main",
+			dark_variant = "main",
+		},
+		init = function()
+			vim.cmd.colorscheme("rose-pine")
+		end,
+	},
 }
